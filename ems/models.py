4aa1ca2e071e3373e29b2fdfe36c54cebaa7b3aa
sunil <sunilyadavwork2003@gmail.com>
2022-08-23 12:10:57 +0530

    created department and role model

--- ems/models.py
+++ ems/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name=models.CharField(max_length=70)
+    description=models.TextField(max_length=200)
+    
+class Role(models.Model):
+    name=models.CharField(max_length=70)
+    description=models.TextField(max_length=200)
+
