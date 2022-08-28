dd0af9688105ed3692e9b0554b3540271fcf654b
sunil <sunilyadavwork2003@gmail.com>
2022-08-28 09:17:27 +0530

    second for test deploy

--- techinterio/settings.py
+++ techinterio/settings.py
@@ -133,7 +133,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
+
+
