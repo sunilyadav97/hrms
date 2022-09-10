860c92203aa85079107ae6d2cdf7689ccaa24544
sunil <sunilyadavwork2003@gmail.com>
2022-09-10 10:03:12 +0530

    crated slider images database

--- home/models.py
+++ home/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class SliderImage(models.Model):
+    image=models.ImageField(upload_to='slider_images')
+    created_at=models.DateTimeField(auto_now_add=True)
