770aa4fb6eac74c72f063b3ee1df1aed79a14048
Sunil Yadav <sunilyadavwork2003@gmail.com>
2022-09-28 11:45:05 +0530

    added created and updated field in document

--- ems/models.py
+++ ems/models.py
@@ -73,7 +73,8 @@ class Document(models.Model):
     employee=models.ForeignKey(Employee,on_delete=models.CASCADE)
     name=models.CharField(max_length=40,null=True,blank=True)
     document=models.FileField(upload_to='employee_documents',null=True,blank=True)
-    
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True,null=True)
     def __str__(self):
         return self.employee.name
 
